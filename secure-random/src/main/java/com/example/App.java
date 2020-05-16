15785f0cca36f66b5377a5e14e846fadf4f6ce7f
ikeyat <Tomoyuki.Ikeya@nttdata.com>
2020-05-16 18:33:39 +0900

    Modify to Spring Boot

--- secure-random/src/main/java/com/example/App.java
+++ secure-random/src/main/java/com/example/App.java
@@ -3,17 +3,26 @@ package com.example;
 import java.security.Provider;
 import java.security.SecureRandom;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SecureRandom secureRandom = new SecureRandom();
-        Provider provider = secureRandom.getProvider();
-        System.out.println("getProvider().getName(): " + provider.getName());
-        System.out.println("getAlgorithm()" + secureRandom.getAlgorithm());
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@GetMapping("/secure-random")
+	public String secureRandom() {
+		SecureRandom secureRandom = new SecureRandom();
+		Provider provider = secureRandom.getProvider();
+		String value = "getProvider().getName(): " + provider.getName() + "<br> getAlgorithm():"
+				+ secureRandom.getAlgorithm();
+		return value;
+	}
 }
+
